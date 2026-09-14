4efae73643709f8f779045248225116ebb314534
Tiagliveira <tiagliveira92@gmail.com>
2026-09-14 05:47:44 -0300

    fix: update API URL endpoint to app.econverse.com.br

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,21 @@
 import axios from 'axios';
-import type { ApiResponse, Product } from './../types/types';
+import type { ApiResponse, Product } from '../types/types';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL =
+    import.meta.env.VITE_API_URL ||
+    "https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json";
+
 export const fetchProducts = async (): Promise<Product[]> => {
     try {
         const response = await axios.get<ApiResponse>(API_URL);
-        return response.data.products || [];
+
+        if (response.data && Array.isArray(response.data.products)) {
+            return response.data.products;
+        }
+
+        return [];
     } catch (error) {
-        console.error('Falha ao buscar produtos da API com Axios:', error);
+        console.error('Falha ao buscar produtos da API Econverse:', error);
         return [];
     }
 };
